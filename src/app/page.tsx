da2b99935d08bbc686864e805b6b703da5198005
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-02-27 13:10:17 -0800

    Adding Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import Header from "components/Header"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-blue-500">Hey</h1>
+      <Header />
     </main>
   )
 }
