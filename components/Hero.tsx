7da72818219f8a2c452ed15efeda837326282463
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-02-27 14:31:17 -0800

    Hero
    Progress to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,27 @@
 'use client'
 import React from 'react'
+import { Cursor, useTypewriter } from "react-simple-typewriter"
+import BackgroundCircles from "./BackgroundCircles"
 
 type Props = {}
 
 export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, I'm Tony!",
+      "Food-Enjoyer.tsx",
+      "<SoftwareDeveloper />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  })
   return (
-    <div>Hero</div>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-central overflow-hidden">
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#FF0000" />
+      </h1>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
     </main>
