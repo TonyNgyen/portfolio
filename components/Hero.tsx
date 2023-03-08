d65bd329b83dbaeaef1257403f70b1b7b4997d6d
Tony <93945730+TonyNgyen@users.noreply.github.com>
2023-03-07 18:35:59 -0800

    About Section
    Adding to about section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import { Cursor, useTypewriter } from "react-simple-typewriter"
 import BackgroundCircles from "./BackgroundCircles"
+import Image from "next/image"
+import picture from "../public/picture.png"
+import Link from "next/link"
 
 type Props = {}
 
@@ -18,10 +21,29 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-central overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
+      {/* UPDATE WITH NEXTJS IMAGE */}
+      <Image src={picture} alt="Picture of Tony" className="relative rounded-full h-32 w-32 mx-auto items-center justify-center"/>
+      <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px] z-30">
+        Software Engineer
+      </h2>
+      <h1 className="text-5xl lg:6xl font-semibold px-10 z-30">
+        <span className="mr-3">{text}</span>
         <Cursor cursorColor="#FF0000" />
       </h1>
+      <div className="pt-5 z-30">
+        <Link href="#about">
+          <button className="heroButton">About</button>
+        </Link>
+        <Link href="#experience">
+          <button className="heroButton">Experience</button>
+        </Link>
+        <Link href="#skills">
+          <button className="heroButton">Skills</button>
+        </Link>
+        <Link href="#projects">
+          <button className="heroButton">Projects</button>
+        </Link>
+      </div>
     </div>
   )
 }
